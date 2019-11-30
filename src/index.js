7ab4b45d19916c6309ca5043214b5f0ff4d1bd0e
Nhat Nguyen <natt.lefevre@gmail.com>
2019-11-30 00:09:18 -0800

    :bulb: Documenting source code

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import * as THREE from 'three';
 
+// setup
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setClearColor('#e5e5e5');
-
 document.body.appendChild(renderer.domElement);
+renderer.setClearColor('#757575');
+renderer.render(scene, camera);
 
+// resize: make canvas full height and width
 window.addEventListener('resize', () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
 });
-
-renderer.render(scene, camera);
