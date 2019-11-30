8a11c32675b873a20c642f2993d14dd11ce6c831
Nhat Nguyen <natt.lefevre@gmail.com>
2019-11-29 19:56:10 -0800

    :art: Improving structure / format of the code

--- src/index.js
+++ src/index.js
@@ -1,37 +1,16 @@
 import * as THREE from 'three';
 
-var camera, scene, renderer;
-var geometry, material, mesh;
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setClearColor('#e5e5e5');
 
-init();
-animate();
+document.body.appendChild(renderer.domElement);
 
-function init() {
-
-  camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
-  camera.position.z = 1;
-
-  scene = new THREE.Scene();
-
-  geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-  material = new THREE.MeshNormalMaterial();
-
-  mesh = new THREE.Mesh(geometry, material);
-  scene.add(mesh);
-
-  renderer = new THREE.WebGLRenderer({ antialias: true });
+window.addEventListener('resize', () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+});
 
-}
-
-function animate() {
-
-  requestAnimationFrame(animate);
-
-  mesh.rotation.x += 0.01;
-  mesh.rotation.y += 0.02;
-
-  renderer.render(scene, camera);
-
-}
+renderer.render(scene, camera);
